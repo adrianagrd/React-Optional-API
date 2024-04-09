053d16cc020d74cf12ebee1d83105c6156df0486
Adriana Garcia <adrianagarciarodriguezzz@gmail.com>
2024-04-09 23:28:41 +0200

    Refactor code: delete unused files and components

--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { StudentList } from './StudentList';
-export { StudentForm } from './StudentForm';
--- src/hooks/index.js
+++
@@ -1,2 +0,0 @@
-export { useStudent } from './useStudent';
-export { useForm } from './useForm';
